49a7ffa01d3e9771888f8926057a6cb113c6b8f4
0xNooodle <0xNoodle@proton.me>
2025-12-05 15:01:14 +0100

    refactor: rename `MIN_EPOCH_DURATION` to `ONE_DAY`

--- src/policies/BaseRewardDistributor.sol
+++ src/policies/BaseRewardDistributor.sol
@@ -29,8 +29,8 @@ abstract contract BaseRewardDistributor is Policy, PolicyEnabler, IRewardDistrib
     /// @notice Role that can update merkle roots
     bytes32 public constant ROLE_MERKLE_UPDATER = "rewards_merkle_updater";
 
-    /// @notice Minimum epoch duration
-    uint40 public constant MIN_EPOCH_DURATION = 1 days;
+    /// @notice One day in seconds
+    uint40 public constant ONE_DAY = 1 days;
 
     /// @notice The TRSRY module
     TRSRYv1 internal TRSRY;
@@ -145,8 +145,8 @@ abstract contract BaseRewardDistributor is Policy, PolicyEnabler, IRewardDistrib
         // Validate epochEndDate is at 23:59:59 UTC (end of day)
         _validateEpochEndOfDay(epochEndDate_);
 
-        // Validate epochEndDate is at least 1 epoch after lastEpochEndDate
-        if (epochEndDate_ < lastEpochEndDate + MIN_EPOCH_DURATION) {
+        // Validate epochEndDate is at least 1 day after lastEpochEndDate
+        if (epochEndDate_ < lastEpochEndDate + ONE_DAY) {
             revert RewardDistributor_EpochTooEarly();
         }
 
@@ -247,14 +247,14 @@ abstract contract BaseRewardDistributor is Policy, PolicyEnabler, IRewardDistrib
     ///
     /// @param  epochStartDate_ The epoch start date to validate
     function _validateEpochStartOfDay(uint256 epochStartDate_) internal pure {
-        if (epochStartDate_ % MIN_EPOCH_DURATION != 0) revert RewardDistributor_EpochNotEndOfDay();
+        if (epochStartDate_ % ONE_DAY != 0) revert RewardDistributor_EpochNotEndOfDay();
     }
 
     /// @notice Validate that an epoch end date is at 23:59:59 UTC (end of day)
     ///
     /// @param  epochEndDate_ The epoch end date to validate
     function _validateEpochEndOfDay(uint256 epochEndDate_) internal pure {
-        if (epochEndDate_ % MIN_EPOCH_DURATION != MIN_EPOCH_DURATION - 1)
+        if ((epochEndDate_ + 1) % ONE_DAY != 0)
             revert RewardDistributor_EpochNotEndOfDay();
     }
 
